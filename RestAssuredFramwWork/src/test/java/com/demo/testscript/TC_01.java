d0f5bae7644ca5f557af0f5269e0dcd738842c6a
kunal <kunal.dhote7799@gmail.com>
2019-02-06 14:22:24 +0530

    Verifying using Id

--- RestAssuredFramwWork/src/test/java/com/demo/testscript/TC_01.java
+++ RestAssuredFramwWork/src/test/java/com/demo/testscript/TC_01.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.demo.pojo.TittleTest;
@@ -18,23 +19,21 @@ import io.restassured.response.Response;
 
 public class TC_01 {
 	Response responce;
-	ArrayList<String> getTitle;
+	ArrayList<Object> getId;
+	ArrayList<Object> getTitle;
+	ArrayList<Object> getAuthor;
+	
 	
 	@BeforeClass
 	
 	public void setUp()
 	{
-		
-	getTitle = new ArrayList<String>()	;
 	
-	getTitle.add("json-server");
-	getTitle.add("kunal");
-	getTitle.add("Jigar");
-	getTitle.add("mukesh");
+		getId = new ArrayList<Object>();
+	    getTitle = new ArrayList<Object>()	;
+	    getAuthor = new ArrayList<Object>();
 	
-	getTitle.add("Nilesh");
-	getTitle.add("Dhaval");
-	getTitle.add("Mitthal");
+	
 	
 	}
 	
@@ -56,6 +55,12 @@ public class TC_01 {
 			
 			for(int i =1;i<=titletest.length;i++)
 			{
+				getId.add(titletest[i-1].getId());
+				
+				getTitle.add(titletest[i-1].getTitle());
+				
+				getAuthor.add(titletest[i-1].getAuthor());
+				
 				System.out.println(getTitle.get(i-1)+titletest[i-1].getTitle());
 				Assert.assertEquals(getTitle.get(i-1), titletest[i-1].getTitle());	
 				
@@ -65,7 +70,44 @@ public class TC_01 {
 		}
 	}
 	
+	@Test(priority=2 , dataProvider="dataApi")
+	public void verifyIdData(String idd , String titlee , String author)
+	{
+		String url = URL.fixUrl+EndPoint.ADD_TITLE.getResouecePath(idd)	;
+		
+		System.out.println(url);
+			responce = WebServiceTest.getMethod(url);
+			Gson gson = new GsonBuilder().create();
+	        TittleTest titletest;
+	        if(responce.getStatusCode()==200)
+			{
+				
+				titletest = gson.fromJson(responce.getBody().asString(), TittleTest.class);
+				
+			Assert.assertEquals(titlee, titletest.getTitle());
+			
+			Assert.assertEquals(author, titletest.getAuthor());
+			
+			}
+	}
 	
+	@DataProvider(name="dataApi")
+	public Object[][] getIdVerification()
+	{
+		
+	Object[][] result = new Object[getId.size()][3];	
+	for(int i=0;i<getId.size();i++)
+	{
+	result[i][0]= getId.get(i);
+	result[i][1]= getTitle.get(i);
+	result[i][2]= getAuthor.get(i);
+	
+	
+		
+	}
+	return result;	
+	}
+}
 /*@Test(priority=2)
 	
 	public void postTest()
@@ -83,4 +125,46 @@ public class TC_01 {
 	System.out.println(responce.getBody().asString());
 	}*/
 
+	
+/*	@Test(priority=2 ,dataProvider="testingp")
+	public void testverfitywithId(String idd , String titlee , String autorr)
+	{
+   String url = URL.fixUrl+EndPoint.ADD_TITLE.getResouecePath(idd)	;
+		
+		   System.out.println(url);
+			responce = WebServiceTest.getMethod(url);
+			Gson gson = new GsonBuilder().create();
+	        TittleTest titletest;
+	        
+	        if(responce.getStatusCode()==200)
+			{
+				
+				titletest = gson.fromJson(responce.getBody().asString(), TittleTest.class);
+				
+				//Assert.assertEquals(id, titletest.getId());
+				Assert.assertEquals(titlee, titletest.getTitle());
+				Assert.assertEquals(autorr, titletest.getAuthor());
+			}	
+			
+		
+	}
+	@DataProvider(name ="testingp")
+	public Object[][] verfitywithId()
+	{
+		Object[][] result = new Object[getId.size()][3];
+		
+		for(int i=0;i<getId.size();i++)
+		{
+			
+			result[i][0] = getId.get(i);
+			result[i][1] = getTitle.get(i);
+			result[i][2] = getAuthor.get(i);
+			
+		}
+		return result;
+		
+		
+	
+	
 }
+}*/
